d7ae6aecdb0fcd36e5c538161a4aeee922048ee1
Kevin Standaert <ks.standaert@gmail.com>
2024-07-03 11:16:01 +0200

    feature: Header add burger menu and nav balise

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,29 @@
-import React from "react";
+"use client";
+
+import { React, useState } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMartiniGlass,
   faWandMagicSparkles,
   faShop,
+  faBars,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  const handleLinkClick = () => {
+    setTimeout(() => {
+      setIsMenuOpen(false);
+    }, 300);
+  };
+
   return (
-    <header className="from-lily-500 sticky top-0 z-50 flex items-center justify-between bg-gradient-to-r to-black px-4 text-sm font-bold">
+    <header className="sticky top-0 z-50 flex items-center justify-between bg-gradient-to-r from-lily-500 to-black px-4 text-sm font-bold">
       <Link className="transition-all hover:scale-105 active:blur-sm" href="/">
         <Image
           className="-mb-3 -mt-3"
@@ -21,41 +34,108 @@ const Header = () => {
         />{" "}
       </Link>
 
-      <ul className="mr-2 flex items-center gap-6">
-        <li>
-          <Link className="flex items-center" href="/">
-            <FontAwesomeIcon
-              className="text-serria-500 mr-2 w-8"
-              icon={faMartiniGlass}
-            />
-            <span className="hover:text-serria-500 active:text-serria-700 transition-all hover:scale-105">
-              Recettes de cocktails
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link className="flex items-center" href="/">
-            <FontAwesomeIcon
-              className="text-serria-500 mr-2 w-8"
-              icon={faWandMagicSparkles}
-            />
-            <span className="hover:text-serria-500 active:text-serria-700 transition-all hover:scale-105">
-              L&apos;art du cocktail
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link className="flex items-center" href="/">
-            <FontAwesomeIcon
-              className="text-serria-500 mr-2 w-8"
-              icon={faShop}
-            />
-            <span className="hover:text-serria-500 active:text-serria-700 transition-all hover:scale-105">
-              Où acheter ?
-            </span>
-          </Link>
-        </li>
-      </ul>
+      {/* menu mobile screen */}
+      <div className="relative md:hidden">
+        <button
+          className={`${isMenuOpen ? "hidden" : "block"} p-2`}
+          onClick={toggleMenu}
+        >
+          <FontAwesomeIcon
+            className="mr-2 h-8 w-8 text-serria-500"
+            icon={faBars}
+          />
+        </button>
+      </div>
+
+      <div
+        className={`${isMenuOpen ? "flex" : "hidden"} fixed inset-0 z-10 mx-8 mt-[25%] h-[50%] items-center justify-center rounded-lg bg-serria-200 bg-opacity-90 p-4 text-lily-950`}
+      >
+        <button
+          className={`${isMenuOpen ? "block" : "hidden"} absolute right-2 top-4 p-2`}
+          onClick={toggleMenu}
+        >
+          <FontAwesomeIcon
+            className="mr-2 h-8 w-8 text-serria-500"
+            icon={faXmark}
+          />
+        </button>
+        <nav>
+          <ul className="flex flex-col gap-8 text-xl">
+            <li onClick={handleLinkClick}>
+              <Link className="flex items-center" href="/">
+                <FontAwesomeIcon
+                  className="mr-2 w-8 text-serria-500"
+                  icon={faMartiniGlass}
+                />
+                <span className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700">
+                  Recettes de cocktails
+                </span>
+              </Link>
+            </li>
+            <li onClick={handleLinkClick}>
+              <Link className="flex items-center" href="/">
+                <FontAwesomeIcon
+                  className="mr-2 w-8 text-serria-500"
+                  icon={faWandMagicSparkles}
+                />
+                <span className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700">
+                  L&apos;art du cocktail
+                </span>
+              </Link>
+            </li>
+            <li onClick={handleLinkClick}>
+              <Link className="flex items-center" href="/">
+                <FontAwesomeIcon
+                  className="mr-2 w-8 text-serria-500"
+                  icon={faShop}
+                />
+                <span className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700">
+                  Où acheter ?
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* menu full screen */}
+      <nav className="hidden items-center gap-6 md:flex">
+        <ul className="mr-2 flex items-center gap-6">
+          <li>
+            <Link className="flex items-center" href="/">
+              <FontAwesomeIcon
+                className="mr-2 w-8 text-serria-500"
+                icon={faMartiniGlass}
+              />
+              <span className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700">
+                Recettes de cocktails
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link className="flex items-center" href="/">
+              <FontAwesomeIcon
+                className="mr-2 w-8 text-serria-500"
+                icon={faWandMagicSparkles}
+              />
+              <span className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700">
+                L&apos;art du cocktail
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link className="flex items-center" href="/">
+              <FontAwesomeIcon
+                className="mr-2 w-8 text-serria-500"
+                icon={faShop}
+              />
+              <span className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700">
+                Où acheter ?
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
