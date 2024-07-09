757fb3596bf3d6a42c49bf6e46752c352ba1b49d
Kevin Standaert <ks.standaert@gmail.com>
2024-07-09 09:23:48 +0200

    add link to portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center gap-4 p-4 text-center text-xs text-white">
-      <p>© 2024 Kevin Standaert - Tous droits réservés</p>
+    <footer className="flex items-center justify-center gap-4 p-4 text-center text-xs text-white">
+      <p>
+        © 2024{" "}
+        <Link
+          target="_blank"
+          className="transition-all hover:scale-105 hover:text-serria-500 active:text-serria-700"
+          href="https://kevinstandaert.github.io/"
+        >
+          Kevin Standaert
+        </Link>{" "}
+        - Tous droits réservés
+      </p>
     </footer>
   );
 };
