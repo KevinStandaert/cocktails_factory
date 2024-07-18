f1080bca1fac359c4aaab0d49f5482255b2afc96
kevin standaert <ks.standaert@gmail.com>
2024-07-18 12:03:35 +0200

    Update responsive and add bg for first card

--- src/app/recipes/[id]/page.js
+++ src/app/recipes/[id]/page.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import useFetchData from "../../../utils/apiClient";
+import Image from "next/image";
 
 export const dynamic = "force-static";
 
@@ -19,23 +20,22 @@ const RecipesIdPage = ({ params: { id } }) => {
   }
 
   return (
-    <div className="min-h-screen items-center p-6 md:p-36">
-      <div className="text-center">
-        <h1 className="mb-20 text-2xl font-bold sm:mb-36 md:text-6xl">
+    <div className="container mx-auto p-6">
+      <div className="w-full text-center">
+        <h1 className="mb-20 mt-10 lg:mt-24 text-center text-4xl font-bold sm:mb-36 md:text-6xl">
           {cocktail.name}
         </h1>
-        <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:grid-rows-4 sm:gap-20">
-          <div className="h-[500px] md:h-[600px] bg-green-500"></div>
-          <div className="h-[500px] sm:h-[600px] bg-red-500"></div>
-          <div className="h-[500px] sm:h-[600px] bg-orange-500"></div>
-          <div className="h-[500px] sm:h-[600px] bg-blue-500"></div>
-          <div className="sm:col-span-2 h-[500px] sm:h-[450px] bg-indigo-500"></div>
-          <div className="sm:col-span-2 h-[500px] sm:h-[450px] bg-pink-500"></div>
+
+        <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:grid-rows-4 lg:gap-20">
+          <div className="h-[500px] max-h-[600px] bg-[url('/cocktail1.jpg')] bg-cover bg-center md:h-[600px]"></div>
+          <div className="h-[500px] max-h-[600px] bg-red-500 lg:h-[600px]"></div>
+          <div className="h-[500px] max-h-[600px] bg-orange-500 lg:h-[600px]"></div>
+          <div className="h-[500px] max-h-[600px] bg-blue-500 lg:h-[600px]"></div>
+          <div className="h-[300px] bg-indigo-500 lg:col-span-2 lg:h-[450px]"></div>
+          <div className="h-[300px] bg-pink-500 lg:col-span-2 lg:h-[450px]"></div>
         </div>
       </div>
     </div>
-
-    
   );
 };
 
