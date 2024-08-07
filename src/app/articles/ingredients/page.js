691e0b09e931fae2e669b70284fb1555aa061c26
kevin standaert <ks.standaert@gmail.com>
2024-08-07 11:21:53 +0200

    feature maerials id page

--- src/app/articles/ingredients/page.js
+++ src/app/articles/ingredients/page.js
@@ -1,18 +1,15 @@
 import React from "react";
-import CocktailCard from "../../../components/CocktailCard";
 
-const Recipes = () => {
-  const dataUrl = "/recipes/cards";
-
+const IngredientsList = () => {
   return (
     <div className="flex min-h-screen flex-col items-center p-12">
       <div className="flex flex-col gap-2 text-center">
-        <div className="grid gap-20 md:grid-cols-2 xl:grid-cols-3">
-          <CocktailCard dataUrl={dataUrl} />
+        <div className="grid gap-20 md:grid-cols-2 lg:grid-cols-3">
+        Coming soon...
         </div>
       </div>
     </div>
   );
 };
 
-export default Recipes;
+export default IngredientsList;
--- src/app/articles/materials/[id]/page.js
+++ src/app/articles/materials/[id]/page.js
@@ -20,8 +20,72 @@ const ArticleIdPage = ({ params: { id } }) => {
 
   return (
     <div className="container mx-auto mt-8 rounded-xl bg-serria-300 bg-opacity-10 p-6">
-      <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:grid-rows-3 lg:gap-20">
-        {material.name}
+      <div className="w-full text-center">
+        <h1 className="mb-20 mt-10 text-4xl font-bold sm:mb-36 md:text-6xl lg:mt-24">
+          {material.name}
+        </h1>
+
+        <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:grid-rows-3 lg:gap-20">
+          {/*first card*/}
+          <div
+            className="h-[250px] rounded-xl bg-cover bg-center shadow-xl shadow-black md:h-[450px] lg:h-[500px] xl:h-[700px]"
+            style={{ backgroundImage: `url(/${material.url_image_1})` }}
+          ></div>
+
+          {/*second card*/}
+          <div className="flex h-[250px] flex-col items-center rounded-xl bg-serria-300 bg-opacity-10 px-2 md:h-[450px] lg:h-[500px] xl:h-[700px]">
+            <h2 className="mb-6 mt-8 flex text-xl font-bold sm:text-3xl">
+              <strong className="text-serria-500">Qu&apos;est-ce </strong>
+              &nbsp;que c&apos;est ?
+            </h2>
+
+            <div className="flex h-[85%] flex-col items-start justify-center px-2">
+              <p className="mb-8 overflow-y-auto p-2 text-justify text-sm sm:text-base md:p-4 lg:text-xl">
+                {material.description}
+              </p>
+            </div>
+          </div>
+
+          {/*third card*/}
+          <div className="flex h-[250px] flex-col items-center rounded-xl bg-serria-300 bg-opacity-10 px-2 md:h-[450px] lg:h-[500px] xl:h-[700px]">
+            <h2 className="mb-6 mt-8 flex text-xl font-bold sm:text-3xl">
+              Comment l&apos;
+              <strong className="text-serria-500">utiliser</strong>&nbsp;?
+            </h2>
+
+            <div className="flex h-[85%] flex-col items-start justify-center px-2">
+              <p className="mb-8 overflow-y-auto p-2 text-justify text-sm sm:text-base md:p-4 lg:text-xl">
+                {material.use}
+              </p>
+            </div>
+          </div>
+
+          {/*fourth card*/}
+          <div
+            className="h-[250px] rounded-xl bg-cover bg-center shadow-xl shadow-black md:h-[450px] lg:h-[500px] xl:h-[700px]"
+            style={{ backgroundImage: `url(/${material.url_image_2})` }}
+          ></div>
+
+          {/*fifth card*/}
+          <div
+            className="h-[250px] rounded-xl bg-cover bg-center shadow-xl shadow-black md:h-[450px] lg:h-[500px] xl:h-[700px]"
+            style={{ backgroundImage: `url(/${material.url_image_3})` }}
+          ></div>
+
+          {/*sixth card*/}
+          <div className="flex h-[250px] flex-col items-center rounded-xl bg-serria-300 bg-opacity-10 px-2 md:h-[450px] lg:h-[500px] xl:h-[700px]">
+            <h2 className="mb-6 mt-8 flex text-xl font-bold sm:text-3xl">
+              Et si j&apos;
+              <strong className="text-serria-500">en ai pas</strong>&nbsp;?
+            </h2>
+
+            <div className="flex h-[85%] flex-col items-start justify-center px-2">
+              <p className="mb-8 overflow-y-auto p-2 text-justify text-sm sm:text-base md:p-4 lg:text-xl">
+                {material.alternative}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
