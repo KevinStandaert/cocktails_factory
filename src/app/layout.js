5618570ce802a9c7f764ef7d2df2c087d5364906
kevin standaert <ks.standaert@gmail.com>
2024-09-04 12:33:56 +0200

    update verification google

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,8 @@ export const metadata = {
   title: "Cocktails Factory - Fabrique à Cocktails",
   description:
     "Découvrez notre collection de recettes de cocktails uniques et savoureuses. Que vous soyez un amateur de cocktails classiques ou à la recherche de nouvelles créations, notre site vous offre des idées inspirantes, des conseils de préparation et des informations sur les ingrédients. Rejoignez-nous pour explorer le monde fascinant des cocktails et préparer des boissons délicieuses chez vous.",
-  meta: {
-    name: "google-site-verification",
-    content: "5eyIXp4bdeWId8K0CXzhtRb8ieAoUTajaJNvk9AKJQM",
+  verification: {
+    google: "5eyIXp4bdeWId8K0CXzhtRb8ieAoUTajaJNvk9AKJQM",
   },
 };
 
