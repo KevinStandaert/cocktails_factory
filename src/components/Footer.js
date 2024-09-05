9658ea79f431ca1836246a1cde63fc7c5977f22d
kevin standaert <ks.standaert@gmail.com>
2024-09-05 09:55:16 +0200

    add alcohol advice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,33 @@
 import Link from "next/link";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="mb-8 flex items-center justify-center p-4 text-center text-xs text-white sm:text-base">
-      
+    <footer>
+      <div className="mb-8 flex flex-col flex-wrap items-center justify-center gap-4 p-4 text-center text-xs text-white sm:text-base">
         {" "}
+        <div className="mb-8 text-base sm:mb-24 sm:text-lg">
+          <p>
+            L&apos;abus d&apos;alcool est dangereux pour la santé, à consommer
+            avec modération.
+          </p>
+        </div>
         <Link
           target="_blank"
-          className="py-8 transition-all hover:text-serria-500 active:text-serria-700"
-          href="https://kevinstandaert.github.io/"
-        >
-          © 2024 Kévin Standaert - Tous droits réservés
-        </Link>{" "}
-        
-        <Link
-          target="_blank"
-          className="sm:ml-16 ml-8 py-8 transition-all hover:text-serria-500 active:text-serria-700"
+          className="transition-all hover:text-serria-500 active:text-serria-700"
           href="/legal"
         >
           Mentions légales
         </Link>{" "}
-      
+        <Link
+          target="_blank"
+          className="transition-all hover:text-serria-500 active:text-serria-700"
+          href="https://kevinstandaert.github.io/"
+        >
+          © {currentYear} Kévin Standaert - Tous droits réservés
+        </Link>{" "}
+      </div>
     </footer>
   );
 };
