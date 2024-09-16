c71e420fbecf96083ba30f373ce6b07d1fd3f41d
kevin standaert <ks.standaert@gmail.com>
2024-09-16 12:04:05 +0200

    add googleTagManager

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import { Roboto_Mono } from "next/font/google";
 import AgeConfirmationModal from "../components/AgeConfirmationModal";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const roboto = Roboto_Mono({ subsets: ["latin"] });
 
@@ -18,16 +19,17 @@ export const metadata = {
     google: "5eyIXp4bdeWId8K0CXzhtRb8ieAoUTajaJNvk9AKJQM",
   },
   openGraph: {
-    type: 'website',
-    url: 'https://cocktails-factory.vercel.app',
-    title: 'Cocktails Factory - Fabrique à Cocktails',
-    description: 'Découvrez notre collection de recettes de cocktails uniques et savoureuses. Que vous soyez un amateur de cocktails classiques ou à la recherche de nouvelles créations.',
+    type: "website",
+    url: "https://cocktails-factory.vercel.app",
+    title: "Cocktails Factory - Fabrique à Cocktails",
+    description:
+      "Découvrez notre collection de recettes de cocktails uniques et savoureuses. Que vous soyez un amateur de cocktails classiques ou à la recherche de nouvelles créations.",
     images: [
       {
-        url: 'https://cocktails-factory.vercel.app/logo_cocktails_factory.webp',
+        url: "https://cocktails-factory.vercel.app/logo_cocktails_factory.webp",
         width: 1024,
         height: 1024,
-        alt: 'Image représentant Cocktails Factory',
+        alt: "Image représentant Cocktails Factory",
       },
     ],
   },
@@ -36,9 +38,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <GoogleTagManager gtmId="GTM-WJQBZ8B5" />
+
       <body
         className={`${roboto.className} bg-gradient-to-r from-metal-600 to-black text-gray-100`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WJQBZ8B5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         <AgeConfirmationModal />
         <main>{children}</main>
